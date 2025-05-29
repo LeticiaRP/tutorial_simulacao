c41a759adb9956df61e2e20409c1a8bd36d83c1e
Leticia <leticiarp2000@hotmail.com>
2025-05-29 14:59:04 -0300

    chore: create a separate sdf file for describe only the robot model and add the robot_state_publisher in the launch file

--- launch/tutorial_gazebo.launch.py
+++ launch/tutorial_gazebo.launch.py
@@ -15,13 +15,35 @@ def generate_launch_description():
 
     # Add ros_gz_sim -> Convenient launch files and executables for using Gazebo Sim with ROS
     pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
-    pkg_gazebo = get_package_share_directory('tutorial_simulacao')
+    pkg_simulation = get_package_share_directory('tutorial_simulacao')
+
+    sdf_file = os.path.join(pkg_simulation, 'simulation', 'robot.sdf')
+    with open(sdf_file, 'r') as infp:
+        robot_desc = infp.read()
+
 
     gz_sim =    IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
                                               launch_arguments={'gz_args': PathJoinSubstitution([
-                                                                           pkg_gazebo, 'simulation', 'building_robot.sdf'])}.items(),)
+                                                                           pkg_simulation, 'simulation', 'building_robot.sdf'])}.items(),)
     
+
+    # Takes the description and joint angles as inputs and publishes the 3D poses of the robot links
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='both',
+        parameters=[
+            {'use_sim_time': True},
+            {'robot_description': robot_desc},
+        ]
+    )
+
+
+
+
     return LaunchDescription([
-        gz_sim
+        gz_sim, 
+        robot_state_publisher
     ])
